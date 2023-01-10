2e65f26b7eb2fc6629bb6cf2128ef0f059e2c9d8
julien <julienfr112@gmail.com>
2023-01-10 23:03:09 +0100

    first read commit

---
+++ scrap.py
@@ -0,0 +1,18 @@
+from scrappers.orange import orange
+import clickhouse_driver
+import json
+
+ch = clickhouse_driver.Client("localhost")
+
+ch.execute(
+    """--sql
+CREATE TABLE IF NOT EXISTS orange (
+    datetime DateTime default now(),
+    data String
+)
+ENGINE=MergeTree
+ORDER BY datetime
+"""
+)
+
+ch.execute("insert into orange(data) values", [[json.dumps(o)] for o in orange()])
---
+++ server.py
@@ -0,0 +1,33 @@
+import asyncio
+import tornado.web
+import clickhouse_driver
+import pandas, tabulate
+
+ch = clickhouse_driver.Client("localhost")
+
+
+def get_data():
+    return {
+        "orange": ch.query_dataframe(
+            "select JSONExtractInt(data, 'prix') as prix from orange"
+        ).to_html()
+    }
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.template.html", data=get_data())
+
+
+async def main():
+    tornado.web.Application(
+        [
+            (r"/", MainHandler),
+        ],
+        debug=True,
+    ).listen(8888)
+    await asyncio.Event().wait()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
