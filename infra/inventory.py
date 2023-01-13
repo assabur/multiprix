4dd0d8fe342aee873ae4ea2ea3e2348ca20f4b7c
julien <julienfr112@gmail.com>
2023-01-13 09:58:00 +0100

    no strict

--- infra/inventory.py
+++ infra/inventory.py
@@ -1,4 +1,4 @@
-conf = {"ssh_user": "root", "ssh_strict_host_key_checking": False}
+conf = {"ssh_user": "root", "strict_host_key_checking": False}
 try:
     open("id_rsa")
     conf["key"] = "id_rsa"
