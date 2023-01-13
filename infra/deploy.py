58d280be159f65da7c1a1670fd7e6b8502e8314d
julien <julienfr112@gmail.com>
2023-01-13 10:49:22 +0100

    finish CI

--- infra/deploy.py
+++ infra/deploy.py
@@ -11,8 +11,8 @@ pip.packages(["tornado", "pytest-playwright"])
 server.shell(["playwright install"])
 
 # COPY FILES
-files.sync("server", "/root/server")
-files.sync("scrap", "scrap")
+files.sync("../server", "/root/server")
+files.sync("../scrap", "scrap")
 
 # SCHEDULE SCRAP
 server.crontab(
