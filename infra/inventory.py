5afbaed9f9b4a47efe9f5d58e13a63745883579e
julien <julienfr112@gmail.com>
2023-01-13 10:23:56 +0100

    add ci

--- infra/inventory.py
+++ infra/inventory.py
@@ -1,4 +1,4 @@
-conf = {"ssh_user": "root", "strict_host_key_checking": False}
+conf = {"ssh_user": "root", "ssh_known_hosts_file": "known_hosts"}
 try:
     open("id_rsa")
     conf["key"] = "id_rsa"
