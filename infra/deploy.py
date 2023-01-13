9897359b98c938f9c5953064ef673dd4c0cebe6d
julien <julienfr112@gmail.com>
2023-01-13 10:34:27 +0100

    finish CI

--- infra/deploy.py
+++ infra/deploy.py
@@ -1,9 +1,29 @@
-from pyinfra.operations import apt, pip
+from pyinfra.operations import apt, pip, files, server, systemd
+from pathlib import Path
 
+# INSTALLS
 apt.key(keyserver="hkp://keyserver.ubuntu.com:80", keyid="8919F6BD2B48D754")
 add_apt_repo = apt.repo(src="deb https://packages.clickhouse.com/deb stable main")
 apt.packages(
     packages=["clickhouse-server", "clickhouse-client", "nginx", "python3-pip"]
 )
+pip.packages(["tornado", "pytest-playwright"])
+server.shell(["playwright install"])
 
+# COPY FILES
+files.put("server", "server")
+files.put("scrap", "scrap")
+
+# SCHEDULE SCRAP
+server.crontab(
+    "cd /root/scrap && python3 scrap.py",
+    minute=0,
+)
+
+# MAKE A SERVICE FOR SERVER
+sd = files.put(Path(__file__).parent / "mpserver.service", "/etc/systemd/system/")
+systemd.service("mpserver", running=True, enabled=True, restarted=True)
+
+#NGINX
+nginconf = files.put(Path(__file__).parent / "nginx.conf", "/etc/nginx/")
 
